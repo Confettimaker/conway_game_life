18b07f619f9a7606ccdd07c48310998a00cb527a
John Adams <jeapnr@mst.edu>
2018-04-05 21:57:47 -0500

    Moved function bodies to cell.hpp from cell.h

--- cell.h
+++ cell.h
@@ -8,77 +8,14 @@ class Cell
   int y;
   bool alive;
   public:
-    Cell()
-    {
-      neighboors = 0;
-      x = -1;
-      y = -1;
-      alive = false;
-    }
-    Cell(bool _alive, int _x, int _y)
-    {
-      neighboors = 0;
-      alive = _alive;
-      x = _x;
-      y = _y;
-    }
+    Cell();
+    Cell(bool _alive, int _x, int _y);
 
-    bool is_alive()
-    {
-      return alive;
-    }
+    bool is_alive();
 
-    void update(Cell ** grid, int n)
-    {
-      neighboors = 0;
-      //Checking cell to the right
-      if (x < (n - 1))
-      {
-        neighboors += static_cast<int>(grid[y][x + 1].is_alive());
-      }
-      //Checking cell to the left
-      if (x > 0)
-      {
-        neighboors += static_cast<int>(grid[y][x - 1].is_alive());
-      }
-      //Checking cell under current cell
-      if (y < (n - 1))
-      {
-        neighboors += static_cast<int>(grid[y + 1][x].is_alive());
-      }
-      //Checking cell above current cell
-      if (y > 0)
-      {
-        neighboors += static_cast<int>(grid[y - 1][x].is_alive());
-      }
-      //Checking cell to the top-right
-      if ((x < (n - 1)) && (y > 0))
-      {
-        neighboors += static_cast<int>(grid[y - 1][x + 1].is_alive());
-      }
-      //Checking cell to the bottom-right
-      if ((x < (n - 1)) && (y < (n - 1)))
-      {
-        neighboors += static_cast<int>(grid[y + 1][x + 1].is_alive());
-      }
-      //Checking cell to the top-left
-      if ((x > 0) && (y > 0))
-      {
-        neighboors += static_cast<int>(grid[y - 1][x - 1].is_alive());
-      }
-      //Checking cell to the bottom-left
-      if ((x > 0) && (y < (n - 1)))
-      {
-        neighboors += static_cast<int>(grid[y + 1][x - 1].is_alive());
-      }
-
-      if (neighboors < 2 && alive)
-        alive = false;
-      if (neighboors > 3 && alive)
-        alive = false;
-      if (neighboors == 3 && !alive)
-        alive = true;
-    }
+    void update(Cell ** grid, int n);
 };
+
+#include "cell.hpp"
 
 #endif
